e5b6b0bdf0494086746af94f032b0f04a36f3a6c
Ece Su Boy <boyecesu@gmail.com>
2025-07-23 14:57:48 +0300

    Generic class and DAL operations

---
+++ Library_Automation.Entities/DAL/DuyurularDAL.cs
@@ -0,0 +1,16 @@
+﻿using Library_Automation.Entities.Model;
+using Library_Automation.Entities.Model.Contacts;
+using Library_Automation.Entities.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Automation.Entities.DAL
+{
+    public class DuyurularDAL: GenericRepository<KutuphaneContext, Duyurular>
+    {
+        
+    }
+}
---
+++ Library_Automation.Entities/DAL/KullanicilarDAL.cs
@@ -0,0 +1,15 @@
+﻿using Library_Automation.Entities.Model;
+using Library_Automation.Entities.Model.Contacts;
+using Library_Automation.Entities.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Automation.Entities.DAL
+{
+    public class KullanicilarDAL: GenericRepository<KutuphaneContext, Kullanicilar>
+    {
+    }
+}
