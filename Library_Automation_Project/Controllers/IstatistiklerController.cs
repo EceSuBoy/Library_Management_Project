fa6d6d4a08c7f3ce974e0ca53a33ea6d69c1f0ca
Ece Su Boy <boyecesu@gmail.com>
2025-08-01 09:49:25 +0300

    Filtering data and transferring it to Excel

--- Library_Automation_Project/Controllers/IstatistiklerController.cs
+++ Library_Automation_Project/Controllers/IstatistiklerController.cs
@@ -1,7 +1,9 @@
-﻿using Library_Automation.Entities.DAL;
+﻿using ClosedXML.Excel;
+using Library_Automation.Entities.DAL;
 using Library_Automation.Entities.Model.Contacts;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,8 +14,9 @@ namespace Library_Automation_Project.Controllers
     {
         KutuphaneContext context = new KutuphaneContext();
         KullaniciHareketleriDAL KullaniciHareketleriDAL = new KullaniciHareketleriDAL();
-        KullanicilarDAL KullanicilarDAL = new KullanicilarDAL();    
+        KullanicilarDAL KullanicilarDAL = new KullanicilarDAL();
         UyelerDAL UyelerDAL = new UyelerDAL();
+        EmanetKitaplarDAL emanetKitaplarDAL = new EmanetKitaplarDAL();
         // GET: Istatistikler
         public ActionResult Index()
         {
@@ -27,7 +30,7 @@ namespace Library_Automation_Project.Controllers
 
             //Kullanici Sayisi
             var KullaniciSayisiModel = KullanicilarDAL.GetAll(context);
-            ViewBag.Count=KullaniciSayisiModel.Count;
+            ViewBag.Count = KullaniciSayisiModel.Count;
 
             //En cok giris yapan kullanici adi ve giris sayisi
             var model = KullaniciHareketleriDAL.KullaniciGirisSayilari();
@@ -40,6 +43,40 @@ namespace Library_Automation_Project.Controllers
             ViewBag.ToplamKHGSayisi = KullaniciHareketleriDAL.ToplamKHGSayisi;
             ViewBag.AltiAyToplamKHGSayisi = KullaniciHareketleriDAL.AltiAyToplamKHGSayisi;
             return View();
+        }
+
+        public ActionResult ExceleAktar()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Borrowed Books");
+                worksheet.Cell(1, 1).Value = "Id";
+                worksheet.Cell(1, 2).Value = "Member Name";
+                worksheet.Cell(1, 3).Value = "Book Name";
+                worksheet.Cell(1, 3).Value = "Number of Books";
+                worksheet.Cell(1, 5).Value = "Date Borrowed";
+
+                var model = emanetKitaplarDAL.GetAll(context, x => x.KitapIadeTarihi == null, "Uyeler", "Kitaplar");
+
+                int row = 2;
+                foreach (var item in model)
+                {
+                    worksheet.Cell(row, 1).Value = item.Id;
+                    worksheet.Cell(row, 2).Value = item.Uyeler.AdiSoyadi;
+                    worksheet.Cell(row, 3).Value = item.Kitaplar.KitapAdi;
+                    worksheet.Cell(row, 4).Value = item.KitapSayisi;
+                    worksheet.Cell(row, 5).Value = item.KitapAldigiTarihi;
+                    row++;
+                }
+                using (var stream=new MemoryStream()) //verileri ramde sakliyoruz
+                {
+                    workbook.SaveAs(stream);
+                    stream.Position = 0;
+                    return File(stream.ToArray(), "application/vnd.openXmlformats-officedocument.spreadsheetml.Sheet", "EmanetKitaplarSeri.xlsx");
+                }
+
+
+            }
         }
     }
 }
